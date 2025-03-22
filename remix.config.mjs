a4145bfd10317417d810b4d755dfab947fffcf0b
Paul Doros <dorospaul26@gmail.com>
2025-03-22 13:09:38 +0200

    fix push

--- remix.config.mjs
+++ remix.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/.*"],
-  serverModuleFormat: "esm",
+  ignoredRouteFiles: ['**/.*'],
+  serverModuleFormat: 'esm',
   future: {
     v3_fetcherPersist: true,
     v3_lazyRouteDiscovery: true,
     v3_relativeSplatPath: true,
     v3_throwAbortReason: true,
     v3_singleFetch: true,
+    v3_staticApiRoutes: true,
+    v3_headersInLoader: true,
   },
+  serverBuildPath: 'api/index.js',
+  serverPlatform: 'node',
+  serverDependenciesToBundle: [/^remix-utils.*/, /^framer-motion.*/],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
+import { vercelPreset } from '@vercel/remix/vite';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     remix({
-      ignoredRouteFiles: ["**/.*"],
-      serverModuleFormat: "esm",
+      ignoredRouteFiles: ['**/.*'],
+      serverModuleFormat: 'esm',
+      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
   css: {
-    transformer: "lightningcss",
+    transformer: 'lightningcss',
   },
   optimizeDeps: {
-    include: ["tailwindcss"],
+    include: ['tailwindcss'],
   },
   resolve: {
     alias: {
-      "~": "/app",
+      '~': '/app',
     },
   },
 });
