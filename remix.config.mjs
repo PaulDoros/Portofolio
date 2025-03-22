da00279c14794c855519d0663c62bf05f439bb0b
Paul Doros <dorospaul26@gmail.com>
2025-03-22 14:13:16 +0200

    Revert to original working configuration

--- remix.config.mjs
+++ remix.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   cacheDirectory: './node_modules/.cache/remix',
-  ignoredRouteFiles: ['**/.*', '**/*.test.{ts,tsx}'],
+  ignoredRouteFiles: ['**/.*'],
   serverModuleFormat: 'esm',
   future: {
     v3_fetcherPersist: true,
     v3_lazyRouteDiscovery: true,
     v3_relativeSplatPath: true,
     v3_throwAbortReason: true,
+    v3_singleFetch: true,
   },
 };
