5582600a1bb7b0e76c0197bd4e3d84e66235348d
Paul Doros <dorospaul26@gmail.com>
2025-03-22 14:10:19 +0200

    Simplify Vercel configuration and fix static file handling

--- remix.config.mjs
+++ remix.config.mjs
@@ -1,15 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ['**/.*'],
+  cacheDirectory: './node_modules/.cache/remix',
+  ignoredRouteFiles: ['**/.*', '**/*.test.{ts,tsx}'],
   serverModuleFormat: 'esm',
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
-  serverBuildPath: 'build/index.js',
-  serverPlatform: 'node',
   future: {
     v3_fetcherPersist: true,
     v3_lazyRouteDiscovery: true,
     v3_relativeSplatPath: true,
     v3_throwAbortReason: true,
-    v3_singleFetch: true,
   },
 };
