13e38ee6621369dbca2ec5d95e05b62cfee4812c
Paul Doros <dorospaul26@gmail.com>
2025-03-24 15:02:23 +0200

    psuh

--- remix.config.mjs
+++ remix.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  serverBuildTarget: 'vercel',
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.test.{ts,tsx}'],
   serverModuleFormat: 'esm',
--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-import { createRequestHandler } from '@remix-run/node';
+import { createRequestHandler } from '@remix-run/vercel';
 import * as build from '@remix-run/dev/server-build';
 
-// Log startup
-console.log('Starting Remix server...');
-
-export default createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
