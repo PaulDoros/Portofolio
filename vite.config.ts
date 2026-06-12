7f1fe1dfcdff26b0bad66e9bafc82b8b8c828b52
pauldorosjackysrl-sketch <261910173+pauldorosjackysrl-sketch@users.noreply.github.com>
2026-06-13 02:22:55 +0300

    Fix Vercel Remix SSR bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { installGlobals } from '@remix-run/node';
+import { vercelPreset } from '@vercel/remix/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -20,6 +21,7 @@ export default defineConfig({
   plugins: [
     remix({
       ignoredRouteFiles: ['**/.*'],
+      presets: [vercelPreset()],
       serverModuleFormat: 'esm',
       future: {
         v3_fetcherPersist: true,
